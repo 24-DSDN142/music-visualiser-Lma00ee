e73a869e7fe1e4d401243f0056b3789be9f95e05
Lma00ee <lakhena.mao0@gmail.com>
2024-09-10 19:29:35 +1200

    Trying lerp colours
    trying lerp colours once again to try change the colour but nooooooooot working. Idk how to have two counters? It works for the first counter but not the second.

--- music_starter.js
+++ music_starter.js
@@ -59,25 +59,21 @@ let darkPurple2 = color(74, 45, 189, 130)
 //bc of opacity, darkest layer first then light?
 
   //reverse opacity layers for strokes outer pink 
-  noStroke()
+  //drums
+ //super outer stroke matching ring on image 
+  noFill()
+  strokeWeight (20)
+  stroke(Magenta3) 
+  ellipse (circleX[0], circleX[1], drumMap1+200, drumMap1+180);
+
+  //magenta colours of main circle 
   fill (darkMagenta) 
   strokeWeight(40)
   stroke(magentaStroke) 
   ellipse(circleX[0], circleX[1], drumMap1+10, drumMap1+10);
   ellipse(circleX[0], circleX[1], drumMap1+90, drumMap1+90);
-
-   //white glowey
-  //  noFill()
-  //  strokeWeight (3)
-  //  stroke(255, 200) //pink
-  //  ellipse (circleX[0], circleX[1], drumMap1+200, drumMap1+190);
-   
  
-  noFill()
-  strokeWeight (20)
-  stroke(Magenta3) //pink
-  ellipse (circleX[0], circleX[1], drumMap1+200, drumMap1+180);
-
+  //purples
   fill (lightPurple1) 
   strokeWeight (0)
   ellipse (circleX[0], circleX[1], drumMap2, drumMap2);
@@ -88,20 +84,16 @@ let darkPurple2 = color(74, 45, 189, 130)
   fill (lightPurple1) 
   ellipse (circleX[0], circleX[1], drumMap4, drumMap4);
 
-  // noFill()
-  // strokeWeight (20)
-  // stroke(Magenta2) //pink
-  // ellipse (circleX[0], circleX[1], drumMap3, drumMap3);
-  
-  // strokeWeight (20)
-  // stroke(Magenta1) //pink
-  
+
+//bass beat circles  
+//darker purples inner small middle 
   fill (darkPurple) 
   ellipse (circleX[0], circleX[1], bassMap1, bassMap1)
 
   fill (darkPurple) 
   ellipse (circleX[0], circleX[1], bassMap2, bassMap2);
   
+//other beat circles 
   fill (darkPurple1) 
   stroke (64, 68, 194)
   ellipse (circleX[0], circleX[1], otherMap1, otherMap1);
@@ -111,6 +103,7 @@ let darkPurple2 = color(74, 45, 189, 130)
   ellipse (circleX[0], circleX[1], otherMap2, otherMap2);
   
 
+  
   //flower/star in middle 
   fill (Magenta1) 
   strokeWeight(2)
@@ -122,9 +115,9 @@ let darkPurple2 = color(74, 45, 189, 130)
   stroke (Magenta) 
   ellipse (circleX[0], circleX[1], 20, vocalMap1);
 
+  
 
-  if(counter >= 100){
-  //5897
+  if(counter >= 5897){
 
   //cool outer stroke circle colours outlining small inner middle circles/overlaying making a cool effect/colour
   noFill()
@@ -151,13 +144,11 @@ let darkPurple2 = color(74, 45, 189, 130)
   
 
   //flower/star in middle 
-  let ellipse1 = map(vocal, 0, 40, 70, 80)
   fill (Magenta1) 
   strokeWeight(2)
   stroke (Magenta) 
   ellipse (circleX[0], circleX[1], vocalMap1, 20); 
 
-  let ellipse2 = map(vocal, 0, 40, 70, 80)
   fill (Magenta1) 
   strokeWeight(2)
   stroke (Magenta) 
@@ -165,150 +156,92 @@ let darkPurple2 = color(74, 45, 189, 130)
 
   }
 
-  else {
+  
 
-  }
+//   else if ((counter > 100 || counter < 300)) {
 
-  }
- //RHEHRBHEWHR
+//     // if (counter > 100 || counter < 300) {
+//       circleCol = lerpColor(Magenta, darkPurple, map(counter, 100, 300, 0, 1))
+//       circleCol2 = lerpColor(lightPurple1, Magenta2, map(counter, 100, 300, 0, 1))
+//       circleCol3 = lerpColor(darkPurple1, Magenta2, map(counter, 100, 300, 0, 1))
+//       circleCol4 = lerpColor(Magenta1, darkPurple2, map(counter, 100, 300, 0, 1))
 
-
-   //fill (255, map(???, 255, 0))
-
-//   //  rect (1000, 500, 500, 500)
-//    // color (145, 234, 122, map(counter))
-
-//    noStroke()
-//    // strokeWeight (5)
-//    // stroke(255) //white 
-//    // ellipse (circleX[0], circleX [1], drumMap, drumMap);
-//    // noFill()
+//   //reverse opacity layers for strokes outer pink 
+//   //drums
+//  //super outer stroke matching ring on image 
+//   noFill()
+//   strokeWeight (20)
+//   stroke(circleCol) 
+//   ellipse (circleX[0], circleX[1], drumMap1+200, drumMap1+180);
+  
+//   //magenta colours of main circle 
+//   noStroke()
+//   fill (circleCol) 
+//   strokeWeight(40)
+//   stroke(circleCol4) 
+//   ellipse(circleX[0], circleX[1], drumMap1+10, drumMap1+10);
+//   ellipse(circleX[0], circleX[1], drumMap1+90, drumMap1+90);
  
-//    // // fill (255, 150) //pink
-//    // strokeWeight (20)
-//    // stroke(161, 29, 179, 210) //pink 
-//    // ellipse (1350, 750, circle4, circle4);
- 
-//    //reverse opacity layers for strokes outer pink 
-//    fill (circleCol) //pink
-//    strokeWeight (40)
-//    stroke(161, 29, 179, 170) //pink
-//    ellipse (circleX[0], circleX[1], drumMap1+10, drumMap1+10);
- 
-//    // fill (255, 150) //pink
-//    strokeWeight (40)
-//    stroke(Magenta1) //pink
-//    ellipse (circleX[0], circleX[1], drumMap1+90, drumMap1+90);
- 
-//     //white glowey
-//    //  noFill()
-//    //  strokeWeight (3)
-//    //  stroke(255, 200) //pink
-//    //  ellipse (circleX[0], circleX[1], drumMap1+200, drumMap1+190);
+//   //purples
+  
+//   fill (circleCol3) 
+//   strokeWeight (0)
+//   ellipse (circleX[0], circleX[1], drumMap2, drumMap2);
+
+//   fill (circleCol3) 
+//   ellipse (circleX[0], circleX[1], drumMap3, drumMap3);
+
+//   fill (circleCol3) 
+//   ellipse (circleX[0], circleX[1], drumMap4, drumMap4);
+
+
+// //bass beat circles  
+// //darker purples inner small middle 
+//   fill (circleCol2) 
+//   ellipse (circleX[0], circleX[1], bassMap1, bassMap1)
+
+//   fill (circleCol2) 
+//   ellipse (circleX[0], circleX[1], bassMap2, bassMap2);
+  
+// //other beat circles 
+//   fill (circleCol2) 
+//   stroke (64, 68, 194)
+//   ellipse (circleX[0], circleX[1], otherMap1, otherMap1);
+  
+//   fill (circleCol3) 
+//   stroke (64, 68, 194)
+//   ellipse (circleX[0], circleX[1], otherMap2, otherMap2);
+
+//   //flower/star in middle 
+//   fill (circleCol4) 
+//   strokeWeight(2)
+//   stroke (Magenta) 
+//   ellipse (circleX[0], circleX[1], vocalMap1, 20); 
+
+//   fill (circleCol4) 
+//   strokeWeight(2)
+//   stroke (Magenta) 
+//   ellipse (circleX[0], circleX[1], 20, vocalMap1);
     
-//    // fill (255, 150) //pink, outsideerr
-//    noFill()
-//    strokeWeight (20)
-//    stroke(161, 29, 179, 130) //pink
-//    ellipse (circleX[0], circleX[1], drumMap1+200, drumMap1+180);
- 
-  
- 
-//    let circle3 = map(drum, 0, 100, 600, 1100)
-//    // noFill()
-//    fill (circleCol) 
-//    // fill (99, 64, 194, 100)
-//    strokeWeight (0)
-//    stroke(255) //white 
-//    ellipse (circleX[0], circleX[1], drumMap2, drumMap2);
- 
-//    let circle2 = map(drum, 0, 100, 300, 900)
-//    // noFill()
-//    fill (circleCol) 
-//    // strokeWeight (5)
-//    // stroke(255) //white 
-//    ellipse (circleX[0], circleX[1], drumMap3, drumMap3);
- 
-//    let circle1 = map(drum, 0, 100, 220, 700)
-//    fill (circleCol) 
-//    ellipse (circleX[0], circleX[1], drumMap4, drumMap4);
- 
-  
- 
- 
-//    let circle6 = map(bass, 0, 50, 150, 250)
- 
-//    // noFill()
-//    // strokeWeight (20)
-//    // stroke(Magenta2) //pink
-//    // ellipse (circleX[0], circleX[1], drumMap3, drumMap3);
-   
-//    // strokeWeight (20)
-//    // stroke(Magenta1) //pink
-//    fill (80, 36, 173, 150) //purple
-//    // stroke(80, 36, 173)
-//    // strokeWeight(3)
-//    ellipse (circleX[0], circleX[1], bassMap1, bassMap1)
- 
-   
- 
-//    let circle5 = map(bass, 0, 30, 100, 160)
-//    fill (80, 36, 173, 150) //purple
-//    // stroke (80, 36, 173)
-//    ellipse (circleX[0], circleX[1], bassMap2, bassMap2);
-   
-   
-//    let circle8 = map(other, 0, 50, 100, 100)
-//    fill (74, 45, 189, 100) // dark purple 
-//    stroke (64, 68, 194)
-//    ellipse (circleX[0], circleX[1], otherMap1, otherMap1);
-   
- 
-//    let circle7 = map(other, 0, 70, 120, 200)
-//    fill (74, 45, 189, 130) // dark purple
-//    stroke (64, 68, 194)
-//    ellipse (circleX[0], circleX[1], otherMap2, otherMap2);
-   
- 
- 
-//    //flower/star 
-//    let ellipse1 = map(vocal, 0, 40, 70, 80)
-//    fill (circleCol) 
-//    strokeWeight(2)
-//    stroke (161, 29, 179) //dark pink, transparent 
-//    ellipse (circleX[0], circleX[1], vocalMap1, 20); //this is just for weird pattern i made, can be normal circle 
- 
-//    let ellipse2 = map(vocal, 0, 40, 70, 80)
-//    fill (circleCol) 
-//    // strokeWeight(2)
-//    // stroke (255, 100) //white, transparent 
-//    ellipse (circleX[0], circleX[1], 20, vocalMap1);
-//  }
-//  else {
-//  }
-  
+//       }
 
-  // //circle
-  // fill (161, 29, 179, 210) //dark pink 
-  // stroke (161, 29, 179, 210) //pink, transparent 
-  // ellipse (1350, 750, ellipse1, ellipse1); //this is just for weird pattern i made, can be normal circle 
+//   else {
+
+//   }
+
+  }
+
+ 
 
 
 
-  // //circle
-  // fill (161, 29, 179, 210) //dark pink 
-  // stroke (161, 29, 179, 210) //pink, transparent 
-  // ellipse (1350, 750, ellipse1, ellipse1); //this is just for weird pattern i made, can be normal circle 
-
-   
-  
-  
-  
 
 
-//   let circle10 = map(words, 0, 150, 150, 200)
-//  fill (255, 70)
-//  ellipse (200, 250, circle10, circle10)
+  //fill (255, map(???, 255, 0))
+
+//  rect (1000, 500, 500, 500)
+// color (145, 234, 122, map(counter))
+
 
 //   if(counter > 300 && counter < 1000){
 //  fill (0)
